a33b0861c79c6d08cbbae4dd1d390f6bc7a3f522
CDafeuAdams <117130837+CDafeuAdams@users.noreply.github.com>
2022-12-09 00:46:29 -0600

    inserted the battle between the robot and dinosaur onto the battlefield. Hardcoded the dinosaur's attack power

--- Battlefield.py
+++ Battlefield.py
@@ -21,9 +21,18 @@ class Battlefield:
     def battle_phase(self):
         while self.dinosaur.health and self.robot.health > 0:
             print(f"\nLet the Games Begin! {self.dinosaur.health and self.robot.health}")
-          
+        if self.dinosaur.attack_power == 100:
+            self.robot.health -= 100
+        if self.robot.active_weapon == 100:
+            self.dinosaur.health -= 100
+        if self.dinosaur.attack_power == 200:
+            self.robot.health -= 200
 
         pass
 
     def display_winner(self):
+        if self.dinosaur.health > 100:
+            print(f"You are the Winner of the Battlefield!")
+        elif self.robot.health <= 100:
+            print(f"You are the winner of the Battlefield!")
         pass
--- Dinosaur.py
+++ Dinosaur.py
@@ -1,8 +1,8 @@
 class Dinosaur:
 
-    def __init__ (self, name, attack_power):
+    def __init__ (self, name, attack_power:int):
         self.name = name
-        self.attack_power = attack_power(100)
+        self.attack_power = 100
         self.health = 400
 
     def attack (self, robot):
